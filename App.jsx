fd9534e65e5be18e727b1d859aefae439f31f395
abhay9656 <abhay9656gupta@gmail.com>
2024-09-18 13:00:00 +0530

    Update App.jsx and package.json with DrawerNavigation component and new dependencies

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './Screens/Home';
+import DrawerNavigation from './Navigation/DrawerNavigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
       
       <Home />
+      <DrawerNavigation/>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
